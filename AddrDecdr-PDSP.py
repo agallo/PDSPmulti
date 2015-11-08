d55c140d30cffd1074aa3a2a800c15da81448539
agallo <agallo@gwu.edu>
2015-11-07 22:00:27 -0500

    monkeying with the sleep timers

--- AddrDecdr-PDSP.py
+++ AddrDecdr-PDSP.py
@@ -181,8 +181,9 @@ def main():
     setup()
     while True:
         writedisplay(list(strftime("%H:%M:%S")), 0)
-        sleep(1)
+        # sleep(1)
         writedisplay(list(inputstring), 1)
+        sleep(1)
 
 
 main()
